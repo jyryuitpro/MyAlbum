e27016f60f9999029881009201f43802b1cdab82
jyryuitpro <jy.ryu.itpro@gmail.com>
2021-11-18 11:22:31 +0900

    ViewController

--- MyAlbum/ViewController.swift
+++ MyAlbum/ViewController.swift
@@ -14,6 +14,9 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func hello(_ sender: Any) {
+        print("hello")
+    }
+    
 }
 
